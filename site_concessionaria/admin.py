a5f5981bee5ec221f79801b3c7d08e625f5c045f
jlxmns <julioximenesjunior@hotmail.com>
2024-05-23 20:27:33 -0300

    Base do projeto

---
+++ site_concessionaria/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from django.contrib.auth.models import Group
+
+from unfold.admin import ModelAdmin
+
+# Register your models here.
+admin.site.unregister(User)
+admin.site.unregister(Group)
+
+
+@admin.register(User)
+class UserAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(Group)
+class GroupAdmin(ModelAdmin):
+    pass
+
---
+++ site_concessionaria/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SiteConcessionariaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'site_concessionaria'
