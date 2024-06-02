21961b1bcba42222fac649e56323b0ab74a27014
Eduardo Henrique <eduardohcvm@gmail.com>
2024-06-02 11:39:40 -0300

    bloco criado

--- site_concessionaria/urls.py
+++ site_concessionaria/urls.py
@@ -5,4 +5,5 @@ app_name = 'site_concessionaria'
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('search', views.CarSearch.as_view(), name='car_search'),
 ]
--- site_concessionaria/views.py
+++ site_concessionaria/views.py
@@ -21,3 +21,21 @@ class HomeView(TemplateBaseView):
         context['teste'] = "teste"
 
         return context
+
+
+class CarSearch(TemplateBaseView):
+    template_name = "site_concessionaria/carSearch.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        img1 = static('images/suzukivitara.jpg')
+        img2 = static('images/toyotafortuner.jpg')
+        img3 = static('images/toyotaprius.jpg')
+        img4 = static('images/yaris3.png')
+        context['imagens'] = [img1, img2, img3, img4]
+
+        context['teste'] = "teste"
+
+        return context
+
