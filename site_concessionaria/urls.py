644706f6d5ca06a2e57a3abd7831d1d4df97f328
Eduardo Henrique <eduardohcvm@gmail.com>
2024-06-02 22:24:45 -0300

    carSearch terminado, apenas falta alguns ajustes de estilização

--- site_concessionaria/urls.py
+++ site_concessionaria/urls.py
@@ -6,4 +6,5 @@ app_name = 'site_concessionaria'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('search', views.CarSearch.as_view(), name='car_search'),
+    path('map', views.MapView.as_view(), name='map_view'),
 ]
--- site_concessionaria/views.py
+++ site_concessionaria/views.py
@@ -39,3 +39,19 @@ class CarSearch(TemplateBaseView):
 
         return context
 
+    class MapView(TemplateBaseView):
+        template_name = "site_concessionaria/mapSearch.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        img1 = static('images/suzukivitara.jpg')
+        img2 = static('images/toyotafortuner.jpg')
+        img3 = static('images/toyotaprius.jpg')
+        img4 = static('images/yaris3.png')
+        context['imagens'] = [img1, img2, img3, img4]
+
+        context['teste'] = "teste"
+
+        return context
+
